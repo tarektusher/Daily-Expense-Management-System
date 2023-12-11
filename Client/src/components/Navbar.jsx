44e42de4b0d5aa37be74589d79058555a09e9bac
tarektusher <mdtarek168504@gmail.com>
2023-12-11 17:46:29 -0600

    Log in form and log out and register form are updated

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,77 +1,83 @@
 import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import MuiAppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import Badge from "@mui/material/Badge";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import MailIcon from "@mui/icons-material/Mail";
-import NotificationsIcon from "@mui/icons-material/Notifications";
 import MoreIcon from "@mui/icons-material/MoreVert";
+import AccountCircle from '@mui/icons-material/AccountCircle';
 import { useAppStore } from "../AppStore";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
-
+import {LoginRegistrationForm} from "./LoginRegistrationForm"
+import { useState } from 'react';
+import { useAuth } from '../hooks/useAuth';
 const AppBar = styled(MuiAppBar)(({ theme }) => ({
   zIndex: theme.zIndex.drawer + 1,
 }));
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
+// const Search = styled("div")(({ theme }) => ({
+//   position: "relative",
+//   borderRadius: theme.shape.borderRadius,
+//   backgroundColor: alpha(theme.palette.common.white, 0.15),
+//   "&:hover": {
+//     backgroundColor: alpha(theme.palette.common.white, 0.25),
+//   },
+//   marginRight: theme.spacing(2),
+//   marginLeft: 0,
+//   width: "100%",
+//   [theme.breakpoints.up("sm")]: {
+//     marginLeft: theme.spacing(3),
+//     width: "auto",
+//   },
+// }));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
+// const SearchIconWrapper = styled("div")(({ theme }) => ({
+//   padding: theme.spacing(0, 2),
+//   height: "100%",
+//   position: "absolute",
+//   pointerEvents: "none",
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "center",
+// }));
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
+// const StyledInputBase = styled(InputBase)(({ theme }) => ({
+//   color: "inherit",
+//   "& .MuiInputBase-input": {
+//     padding: theme.spacing(1, 1, 1, 0),
+//     // vertical padding + font size from searchIcon
+//     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+//     transition: theme.transitions.create("width"),
+//     width: "100%",
+//     [theme.breakpoints.up("md")]: {
+//       width: "20ch",
+//     },
+//   },
+// }));
 
 export default function Navbar() {
   const updateDopen = useAppStore((state) => state.updateDopen);
   const dopen = useAppStore((state) => state.dopen);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  const { isAuthenticated, onLogout } = useAuth();
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+    const handleClickOpen = () => {
+    setOpen(true);
+  };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
   const handleProfileMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -128,11 +134,21 @@ export default function Navbar() {
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
-       <Button sx={{color : 'black', fontWeight : '600'}} onClick={()=>navigate('about')}>About</Button>
-       <br></br>
-            <Button sx={{color : 'black', fontWeight : '600'}}>LogIn</Button>
-            <br></br>
-            <Button sx={{color : 'black', fontWeight : '600'}}>LogOut</Button>
+      <Button
+        sx={{ color: "black", fontWeight: "600" }}
+        onClick={() => navigate("about")}
+      >
+        About
+      </Button>
+      <br></br>
+      <Button
+        sx={{ color: "black", fontWeight: "600" }}
+        onClick={() => navigate("login")}
+      >
+        LogIn
+      </Button>
+      <br></br>
+      <Button sx={{ color: "black", fontWeight: "600" }}>LogOut</Button>
     </Menu>
   );
 
@@ -165,11 +181,34 @@ export default function Navbar() {
             incoMEX
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <Button sx={{color : 'black', fontWeight : '600'}} onClick={()=>navigate('about')}>About</Button>
-            <Button sx={{color : 'black', fontWeight : '600'}}>LogIn</Button>
-            <Button sx={{color : 'black', fontWeight : '600'}}>LogOut</Button>
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
             
+            {isAuthenticated() && (
+              <Button color="inherit" onClick={() => navigate('/about')}>
+                About
+              </Button>
+            )}
+
+            {isAuthenticated() ? (
+              <Box>
+              <IconButton
+                  size="large"
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-controls={menuId}
+                  aria-haspopup="true"
+                  onClick={handleProfileMenuOpen}
+                  color="inherit"
+              >
+                  <AccountCircle />
+                </IconButton>
+                </Box>
+              ) : (
+                <Button color="inherit" onClick={handleClickOpen}>
+                  Login
+                </Button>
+              )}
+              <LoginRegistrationForm open={open} handleClose={handleClose} />
           </Box>
           <Box sx={{ display: { xs: "flex", md: "none" } }}>
             <IconButton
