256a522b6b024ed21240d79a57b298dfd306859b
tarektusher <mdtarek168504@gmail.com>
2023-12-26 19:27:41 -0600

    Adding Login and Logout settings

--- Client/src/pages/HomePage.jsx
+++ Client/src/pages/HomePage.jsx
@@ -1,40 +1,28 @@
 import React from 'react';
-// import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Box, Typography} from '@mui/material';
 import "../../src/Dash.css";
-// import authServices from '../services/authServices';
+import authServices from '../services/authServices';
 import LeftBar from '../components/LeftBar'
-import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import HomeElement from '../components/HomeElement';
-
+import DashBoard from '../components/DashBoard'
+import LogoutHomepage from '../components/LogoutHomepage'
 export const HomePage = () => {
-  // const location = useLocation();
-  // const isUserLoggedIn = authServices.isUserLoggedIn();
+  const location = useLocation();
+  const isUserLoggedIn = authServices.isUserLoggedIn();
   const val = 1;
   return (
     <>
-      {/* <LeftBar />
+      <Navbar />
       {isUserLoggedIn? 
       <div>
+        <LeftBar/>
         {location.pathname === '/'? <DashBoard/>: <Outlet />}
       </div>
       : 
-        // <LogoutHomepage/>
-        null
+        <LogoutHomepage/>
       }
-       */}
-      <Navbar/>
-      <LeftBar/>
-      {val == 1 ?
-          <Outlet/>
-      : 
-      <Box sx={{display : 'flex'}}  className = "bgColor">
-          <HomeElement/>
-      </Box>}
-      
-      
-      
     </>
   );
 };
